a6b5c0ea00ceb0c4f74d61d717929e777eba801d
shaunisrani <sisrani@ucsd.edu>
2025-11-09 02:43:02 -0800

    assign_good_and_evil.py

--- assign_good_and_evil.py
+++ assign_good_and_evil.py
@@ -1,3 +1,4 @@
+'''
 from dsc40graph import UndirectedGraph
 from collections import deque
 
@@ -48,3 +49,36 @@ def assign_good_and_evil(graph):
                             
     # If the loop finishes with no conflicts, the labeling is successful
     return labels
+'''
+
+from collections import deque
+def assign_good_and_evil(graph):
+    label = {}
+    status = {node: 'undiscovered' for node in graph.nodes}
+    for node in graph.nodes:
+        if status[node] == 'undiscovered':
+            ossible = good_and_evil_bfs(graph, node, status, label)
+            if not possible:
+                return None
+    return label
+def good_and_evil_bfs(graph, source, status, label):
+    status[source] = 'pending'
+    label[source] = 'good'
+    pending = deque([source])
+    # while there are still pending nodes
+    while pending:
+        u = pending.popleft()
+        for v in graph.neighbors(u):
+            # explore edge (u,v)
+            if status[v] == 'undiscovered':
+                status[v] = 'pending'
+                if label[u] == 'good':
+                    label[v] = 'evil'
+                else:
+                    label[v] = 'good'
+                # append to right
+                pending.append(v)
+            elif label[u] == label[v]:
+                return False
+        status[u] = 'visited'
+    return True
